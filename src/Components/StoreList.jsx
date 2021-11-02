9b4bfbd4831298d2124ee41dbb0a4b7acacf4730
Jevgenijus <j.suchodolskis@gmail.com>
2021-11-02 22:13:18 +0200

    update

--- src/Components/StoreList.jsx
+++ src/Components/StoreList.jsx
@@ -5,12 +5,11 @@ function StoreList({crystals, modal}) {
     return (
         <div className="crystals_list">
             <div className="my_store_welcome">  
-            <h2>Welcome to mine small store :D</h2>
+            <h2>Welcome to mine small store ^_^</h2>
             </div>
             <div className="my_store_map">
                 {crystals.map(crystal => <StoreCrystal key={crystal.id} crystal={crystal} modal={modal}></StoreCrystal>)}
-            </div>
-            
+            </div>   
         </div>
     )
 }
